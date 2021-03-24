c4518a6a204356fe6945a744f3d5df4d576c2a74
Rafał Kostecki <edward-12@o2.pl>
2021-03-24 17:59:03 +0100

    POY-115 change page colors

--- client/src/app/components/profile-panel/profile-panel.component.ts
+++ client/src/app/components/profile-panel/profile-panel.component.ts
@@ -46,7 +46,7 @@ export class ProfilPanelComponent {
   }
 
   public settings() {
-    window.open('http://localhost:8080/auth/realms/poyters-account/account/', '_blank');
+    window.open('http://localhost:8080/auth/realms/poyters-account/account?refereer=poyters', '_blank');
   }
 
 }
